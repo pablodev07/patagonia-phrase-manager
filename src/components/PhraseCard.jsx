569686915020c2cb326490260601d0642a6338bf
pablodev07 <thisispablovester@gmail.com>
2025-02-14 20:45:17 -0300

    instala tailwind

--- src/components/PhraseCard.jsx
+++ src/components/PhraseCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const PhraseCard = ({ phrase, onDelete }) => {
   return (
     <div className="phrase-card">
-      <p>{phrase}</p>
+      <p className="text-xl font-bold text-red-500">{phrase}</p>
       <button onClick={onDelete}>Eliminar</button>
     </div>
   );
